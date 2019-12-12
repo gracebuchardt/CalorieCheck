5548d582556c48fdc62bbdd62a8dd94a8f230a84
gracebuchardt <43751546+gracebuchardt@users.noreply.github.com>
2019-12-12 11:14:30 -0500

    stuff
    im

--- src/Main.java
+++ src/Main.java
@@ -41,41 +41,41 @@ public class Main {
 			System.out.println((power)+(" Watts of energy were used."));
 			System.out.println("");
 			System.out.println("A watt is measured in 'joules used per second.'");
-			System.out.println(("For every second, you used ")+(power)+(" joules."));
+			System.out.println(("For every second, you used ")+(power)+(" watts."));
 			
 			System.out.println(" ");
-			System.out.println("This is close to the amount of power required to");
+			System.out.print("This is close to the amount of power required to");
 			
 			
 			//this is saying
 			//:If power is greater than 0 AND power is lower than 200, do x.
 			if (power>0&&power<82) {
-				System.out.println(" sleep.");
+				System.out.print(" sleep.");
 			}
 				
 				if (power>83&&power<150) {
-					System.out.println(" sit at rest.");
+					System.out.print(" sit at rest.");
 					
 			}
 				
 				if (power>151&&power<220) {
-					System.out.println(" sit and study.");
+					System.out.print(" sit and study.");
 				
 				
 			}
 				
 				if (power>221&&power<300) {
-					System.out.println(" walk at 5km/h.");
+					System.out.print(" walk at 5km/h.");
 					
 					
 				}
 				
 				if (power>301&&power<500) {
-					System.out.println(" play tennis.");
+					System.out.print(" play tennis.");
 				}
 				
-				if (power>501&&power<850); {
-				System.out.println(" play basketball.");
+				if (power>501&&power<850) {
+				System.out.print(" play basketball.");
 			
 			//if power=(>400) {
 				//System.out.println();
